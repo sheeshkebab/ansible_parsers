f2ec70faacdb6b7150a5ee45c9062c3459e4c937
ansible <rhailsto@redhat.com>
2019-07-09 12:34:25 +1000

    moving the loading of json inventory to a dynamic function rather than one time import at Vinnis request to have dynamic loading capability

--- microservice/services/cmdb.py
+++ microservice/services/cmdb.py
@@ -9,8 +9,18 @@ app = Flask(__name__)
 database_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 print database_path
 
-with open("{}/database/inventory.json".format(database_path), "r") as f:
-    inv = json.load(f)
+#with open("{}/database/inventory.json".format(database_path), "r") as f:
+#    inv = json.load(f)
+
+def load_inventory():
+    with open("{}/database/inventory.json".format(database_path), "r") as f:
+        inventory = json.load(f)
+    return inventory
+
+#@app.route("/reload", methods=['POST'])
+#def reload_latest_inventory():
+#    load_inventory()
+#    return "reloaded inventory\n" 
 
 @app.route("/", methods=['GET'])
 def hello():
@@ -22,7 +32,7 @@ def hello():
 def all_inventory():
     ''' Returns the list of Serials '''
 
-    resp = make_response(json.dumps(inv, sort_keys=True, indent=4))
+    resp = make_response(json.dumps(load_inventory(), sort_keys=True, indent=4))
     resp.headers['Content-Type']="application/json"
     return resp
 
@@ -31,7 +41,7 @@ def all_inventory():
 def list_keys():
 
     key_list = [] 
-    for k in inv:
+    for k in load_inventory():
         key_list.append(k)
 
     return jsonify(key_list)
@@ -41,20 +51,20 @@ def list_keys():
 def list_keys_value(serial, key):
     ''' Returns info about a specific serial number contents '''
 
-    if serial not in inv:
+    if serial not in load_inventory():
         return "Not found"
 
-    return jsonify(inv[serial][key])
+    return jsonify(load_inventory()[serial][key])
 
 
 @app.route('/inventory/<serial>', methods=['GET'])
 def inventory_data(serial):
     ''' Returns info about a specific serial number '''
 
-    if serial not in inv:
+    if serial not in load_inventory():
         return "Not found"
 
-    return jsonify(inv[serial])
+    return jsonify(load_inventory()[serial])
 
 @app.route('/inventory/<sn>/lists', methods=['GET'])
 def inventory_lists(sn):
